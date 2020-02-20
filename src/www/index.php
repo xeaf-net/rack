d270426b31e89c03f7e5ac62a42a22f1ac76463c
Николай Анохин <n.anokhin@xeaf.net>
2020-02-20 22:29:43 +0300

    Перенос папки www в корень src

--- src/www/index.php
+++ src/www/index.php
@@ -10,18 +10,31 @@
  *
  * @license   Apache 2.0
  */
-namespace XEAF\Space\WWW;
+namespace XEAF\Rack\WWW;
 
 use XEAF\Rack\API\App\Application;
 
-//
-// Глобальные параметры приложения
-//
+/**
+ * Путь к папке с файлами конфигурации
+ */
 define('__XEAF_RACK_CONFIG_DIR__', __DIR__ . '/../etc');
+
+/**
+ * Путь к директории файлов сторонних поставщиков
+ */
 define('__XEAF_RACK_VENDOR_DIR__', __DIR__ . '/../../vendor');
+
+/**
+ * Признак режима отладки
+ */
 define('__XEAF_RACK_DEBUG_MODE__', true);
 
-/** @noinspection PhpIncludeInspection */
+/**
+ * Подгрузка autoload.php
+ */
 require_once __XEAF_RACK_VENDOR_DIR__ . '/autoload.php';
 
+/**
+ * Запуск приложения
+ */
 (new Application())->run();
