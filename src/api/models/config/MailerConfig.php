f1ff39e25335d73aafdfc7f94a66f6f38ea7ddc7
Николай Анохин <n.anokhin@xeaf.net>
2020-03-25 09:25:05 +0300

    Добавлен класс параметров конфигурации отправки почты

---
+++ src/api/models/config/MailerConfig.php
@@ -0,0 +1,226 @@
+<?php declare(strict_types = 1);
+
+/**
+ * MailerConfig.php
+ *
+ * Файл является неотъемлемой частью проекта RACK
+ *
+ * @author    Николай В. Анохин <n.anokhin@xeaf.net>
+ * @copyright 2020 XEAF.NET Group
+ *
+ * @license   Apache 2.0
+ */
+namespace XEAF\Rack\API\Models\Config;
+
+use PHPMailer\PHPMailer\PHPMailer;
+use XEAF\Rack\API\App\Factory;
+use XEAF\Rack\API\Core\ConfigModel;
+use XEAF\Rack\API\Utils\Strings;
+
+/**
+ * Описывает параметры конфигурации отправки электронной почты
+ *
+ * @property-read bool   $smtp     Признак использования SMTP
+ * @property-read string $host     Имя хоста
+ * @property-read int    $port     Номер порта
+ * @property-read bool   $auth     Признак необходимости авторизации
+ * @property-read string $secure   Способ защиты соединения
+ * @property-read string $userName Имя пользователя
+ * @property-read string $password Пароль
+ * @property-read string $sendFrom Отправитель
+ *
+ * @package  XEAF\Rack\API\Models\Config
+ */
+class MailerConfig extends ConfigModel {
+
+    /**
+     * Имя секции файла конфигурации
+     */
+    public const SECTION_NAME = 'mailer';
+
+    /**
+     * Признак использования SMTP по умолчанию
+     */
+    private const DEFAULT_SMTP = false;
+
+    /**
+     * Имя хоста по умолчанию
+     */
+    private const DEFAULT_HOST = 'localhost';
+
+    /**
+     * Номер порта по умолчанию
+     */
+    private const DEFAULT_PORT = 25;
+
+    /**
+     * Признак необходимости авторизации по умолчанию
+     */
+    private const DEFAULT_AUTH = false;
+
+    /**
+     * Способ защиты соединения по умолчанию
+     */
+    private const DEFAULT_SECURE = PHPMailer::ENCRYPTION_STARTTLS;
+
+    /**
+     * Имя пользователя по умолчанию
+     */
+    private const DEFAULT_USERNAME = '';
+
+    /**
+     * Пароль по умолчанию
+     */
+    private const DEFAULT_PASSWORD = '';
+
+    /**
+     * Отправитель по умолчанию
+     */
+    private const DEFAULT_SEND_FROM = '';
+
+    /**
+     * Признак использования SMTP
+     * @var bool
+     */
+    private $_smtp = self::DEFAULT_SMTP;
+
+    /**
+     * Имя хоста по умолчанию
+     * @var string
+     */
+    private $_host = self::DEFAULT_HOST;
+
+    /**
+     * Номер порта по умолчанию
+     * @var int
+     */
+    private $_port = self::DEFAULT_PORT;
+
+    /**
+     * Признак необходимости авторизации
+     * @var bool
+     */
+    private $_auth = self::DEFAULT_AUTH;
+
+    /**
+     * Способ защиты соединения
+     * @var string
+     */
+    private $_secure = self::DEFAULT_SECURE;
+
+    /**
+     * Имя пользователя
+     * @var string
+     */
+    private $_userName = self::DEFAULT_USERNAME;
+
+    /**
+     * Пароль
+     * @var string
+     */
+    private $_password = self::DEFAULT_PASSWORD;
+
+    /**
+     * Отправитель
+     * @var string
+     */
+    private $_sendFrom = self::DEFAULT_SEND_FROM;
+
+    /**
+     * Возвращает признак использования SMTP
+     *
+     * @return bool
+     */
+    public function isSmtp(): bool {
+        return $this->_smtp;
+    }
+
+    /**
+     * Возвращает имя хоста
+     *
+     * @return string
+     */
+    public function getHost(): string {
+        return $this->_host;
+    }
+
+    /**
+     * Возвращает номер порта
+     *
+     * @return int
+     */
+    public function getPort(): int {
+        return $this->_port;
+    }
+
+    /**
+     * Возвращает признак необходимости авторизации
+     *
+     * @return bool
+     */
+    public function getAuth(): bool {
+        return $this->_auth;
+    }
+
+    /**
+     * Возвращает способ защиты соединения
+     *
+     * @return string
+     */
+    public function getSecure(): string {
+        return $this->_secure;
+    }
+
+    /**
+     * Возвращает имя пользователя
+     *
+     * @return string
+     */
+    public function getUserName(): string {
+        return $this->_userName;
+    }
+
+    /**
+     * Возвращает пароль
+     *
+     * @return string
+     */
+    public function getPassword(): string {
+        return $this->_password;
+    }
+
+    /**
+     * Возвращает отправителя
+     *
+     * @return string
+     */
+    public function getSendFrom(): string {
+        return $this->_sendFrom;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function parseConfigurationSection(object $data): void {
+        $strings         = Strings::getInstance();
+        $this->_smtp     = (bool)$data->{'smtp'} ?? self::DEFAULT_SMTP;
+        $this->_host     = $data->{'host'} ?? self::DEFAULT_HOST;
+        $this->_port     = $strings->stringToInteger($data->{'port'} ?? null, self::DEFAULT_PORT);
+        $this->_auth     = (bool)$data->{'auth'} ?? self::DEFAULT_AUTH;
+        $this->_secure   = $data->{'secure'} ?? self::DEFAULT_SECURE;
+        $this->_userName = $data->{'userName'} ?? self::DEFAULT_USERNAME;
+        $this->_password = $data->{'password'} ?? self::DEFAULT_PASSWORD;
+        $this->_sendFrom = $data->{'sendFrom'} ?? self::DEFAULT_SEND_FROM;
+    }
+
+    /**
+     * Возвращает единичный экземпляр объекта класса
+     *
+     * @return \XEAF\Rack\API\Models\Config\MailerConfig
+     */
+    public static function getInstance(): MailerConfig {
+        $result = Factory::getFactoryObject(self::class);
+        assert($result instanceof MailerConfig);
+        return $result;
+    }
+}
